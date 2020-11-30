abed5b1ccfc9b647b8eaacd8dc620cbe95675d56
islambou9427 <islem.bouagada@gmail.com>
2020-11-30 17:11:28 +0100

    finish text form field on app bar

--- flutterweb/lib/Screen/HomeScreen.dart
+++ flutterweb/lib/Screen/HomeScreen.dart
@@ -8,6 +8,48 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(18, 18, 18, 1),
+      appBar: PreferredSize(
+        preferredSize: Size(width, height * 0.09),
+        child: Container(
+          height: height * 0.09,
+          color: Color.fromRGBO(32, 32, 32, 1),
+          child: Stack(
+            children: [
+              Positioned(
+                left: width * 0.231,
+                top: 15,
+                child: Container(
+                  width: width * 0.446,
+                  height: 30,
+                  color: Colors.black,
+                  child: TextFormField(
+                    cursorColor: Colors.white,
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.only(top: 3, left: 10),
+                      hintText: "Recharcher",
+                      hintStyle:
+                          TextStyle(color: Color.fromRGBO(117, 117, 117, 1)),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(1)),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(1),
+                          borderSide: BorderSide(
+                            color: Color.fromRGBO(28, 98, 185, 1),
+                          )),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
